b1d605f0f683e284a804058e27b9fe13f9454e33
YoungBerchman <praiseegbe99@gmail.com>
2024-10-01 03:24:28 +0100

    first commit

---
+++ practiceFE/src/mainRouter.tsx
@@ -0,0 +1,44 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomeScreen from "./HomeScreen";
+import RegisterScreen from "./page/RegisterScreen";
+import QuestionScreen from "./page/QuestionScreen";
+import LoginScreen from "./page/SignIn";
+// import PrivateRoute from "./privateRoute";
+import DisplayScreen from "./page/DisplayScreen";
+import { MainQuestion } from "./page/MainQuestion";
+import SliderComponents from "./components/SliderComponents";
+
+export const mainRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeScreen />,
+  },
+  {
+    path: "/register",
+    element: <RegisterScreen />,
+  },
+  {
+    path: "/login",
+    element: <LoginScreen />,
+  },
+  {
+    path: "/display",
+    element: <DisplayScreen />,
+  },
+  {
+    path: "/main",
+    element: <MainQuestion />,
+  },
+  {
+    path: "/slider",
+    element: <SliderComponents />,
+  },
+  {
+    path: "/question",
+    element: (
+      //   <PrivateRoute>
+      <QuestionScreen />
+      //   </PrivateRoute>
+    ),
+  },
+]);
